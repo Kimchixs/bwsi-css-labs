6b586f89dbeac2f22ccb8e61d8c97b82d68e919c
Kimchixs <vincent.le2009@gmail.com>
2026-03-30 03:57:55 -0400

    Update lab_1a.py
    My personal introduction

--- labs/lab_1/lab_1a.py
+++ labs/lab_1/lab_1a.py
@@ -11,6 +11,6 @@ def main():
     name = "Vincent Le" # TODO: Insert your name between the double quotes
 
     print(f"{name}, Welcome to the CSS course!")
-
+    print("Hi! I am eager to join the CSS course. I look forward to developing computer science skills. I am taking this course as a prerequisite for Medlytics, as I intend to major in nursing and have a fascination with data analysis. The start of the course appears interesting as it is applicable to such a wide range of duties.")
 if __name__ == "__main__":
     main()
